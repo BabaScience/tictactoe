88d96d9f2fabce2c1569be2dfb8a4a8d02994ccc
Anonymous <no-reply@example.com>
2024-09-11 14:47:19 +0200

    first X eventListener

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Frontend application is running!');
-});
+let cells  = document.querySelectorAll('.cells') ;
+let board = document.querySelector('.board');
+let Xturn = true ;
+let Oturn = false ;
+let Xclass = 'x';
+let Oclass = 'o';
+
+
+
+
+cells.forEach(cell => {
+  cell.addEventListener('click',(e)=>{
+    if(Xturn && cell.textContent === ''){
+        cell.innerHTML = Xclass;
+        changeTurn(Xturn)
+
+    }
+  })
+})
+
+function changeTurn(bool){
+   return !bool;
+
+}
