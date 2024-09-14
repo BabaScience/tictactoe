d20a792eecc5fd4be2315a38b3c377b05e91cd42
Anonymous <no-reply@example.com>
2024-09-14 23:21:45 +0200

    almost done

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,24 +3,25 @@ let board = document.querySelector('.board');
 let turn = true;
 let Xclass = `x`;
 let Oclass = 'o';
-let c = 1;
+let c = 0;
 let wText = document.querySelector('.winning-text');
 let cellsBoard = board.children;
-let arrCellsBoard = [...cellsBoard]
+let arrCellsBoard = [...cellsBoard];
+let restartBtn = document.getElementById('restart-btn');
 
 let xcells = [];
 let ocells = [];
 
 // Winning chains
 let arrWinner = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
-  [3, 6, 9],
-  [7, 5, 3],
-  [1, 5, 9]
+  [6, 4, 2],
+  [0, 4, 8]
 ];
 
 let result = '';
@@ -32,6 +33,17 @@ cell.forEach(element => {
 });
 
 
+restartBtn.addEventListener('click',function(){
+  cell.forEach(cell => {
+    cell.classList.remove(Xclass, Oclass,'winnerCells');
+    cell.textContent = '';
+  });
+  xcells = [];
+  ocells = [];
+  wText = '';
+
+})
+
 function checkWin(playerCells) {
 
   return arrWinner.some(combination => combination.every(elem => playerCells.includes(elem)));
@@ -41,20 +53,15 @@ function checkDraw(){
   return arrCellsBoard.every(cell => cell.classList.contains(Xclass) || cell.classList.contains(Oclass))
 }
 
-function resetBoard() {
-  cell.forEach(cell => {
-    cell.classList.remove(Xclass, Oclass);
-    cell.textContent = '';
-  });
-  xcells = [];
-  ocells = [];
-}
 
 function colorCell(ind){
   arrCellsBoard[arrWinner[ind][0]].classList.add('winnerCells');
   arrCellsBoard[arrWinner[ind][1]].classList.add('winnerCells');
   arrCellsBoard[arrWinner[ind][2]].classList.add('winnerCells');
 }
+
+
+
 
 
 cell.forEach(cell => {
@@ -69,39 +76,37 @@ cell.forEach(cell => {
 
       
       if (checkWin(xcells)) {
-
         let index = arrWinner.findIndex(combination => combination.every(arrayNum => xcells.includes(arrayNum)));
         result = 'X wins!';
         wText.textContent = result ;
-        colorCell(index)
-
-      
+        colorCell(index);
+        return;
       }
+
 
       if (checkDraw()){
-        console.log('Draw !');
-        resetBoard();
+        result = 'Draw !'
+        wText.textContent = result ;
       }
-
       turn = false;
 
     } else if (!turn && cell.textContent === '') {
-      
-      cell.classList.add(Oclass);
-      cell.innerHTML = Oclass;
-      ocells.push(parseInt(cell.id)); 
+        cell.classList.add(Oclass);
+        cell.innerHTML = Oclass;
+        ocells.push(parseInt(cell.id)); 
 
      
       if (checkWin(ocells)) {
+        let index = arrWinner.findIndex(combination => combination.every(arrayNum => ocells.includes(arrayNum)));
         result = 'O wins!';
-        console.log(result); 
-        resetBoard()
-        
+        wText.textContent = result ;
+        colorCell(index);
+        return;
       }
 
       if (checkDraw()){
-        console.log('Draw !');
-        resetBoard();
+        result = 'Draw !'
+        wText.textContent = result ;
       }
 
       turn = true;
